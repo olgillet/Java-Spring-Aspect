62d8e8cf9eba5ad3b3c2daf7f79fcdadca3f8f7b
olgillet <65901644+olgillet@users.noreply.github.com>
2020-06-09 00:07:27 +0200

    Add files via upload

--- ajc-spring-aspect-log4j/src/main/java/com/springaspect/music/Application.java
+++ ajc-spring-aspect-log4j/src/main/java/com/springaspect/music/Application.java
@@ -6,7 +6,15 @@ public class Application {
 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext myContext = new AnnotationConfigApplicationContext(AppConfig.class);
-		myContext.getBeanFactory().createBean(ApplicationSpringAspect.class).run();
+		
+		//2 versions : 
+			// 1
+		//myContext.getBeanFactory().createBean(ApplicationSpringAspect.class).run();
+		
+			// 2
+		UserDao userDao = myContext.getBean("userDao", UserDao.class);
+		userDao.addUser("tim", new User("Tim", "Porter", 456456));
+		
 		myContext.close();
 	}
 
